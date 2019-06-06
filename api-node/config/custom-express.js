ce0c650bcf08cf950e56f5879e7b2220f46a7cbd
Ruy Freire <ruy.slv@gmail.com>
2019-06-05 21:45:25 -0300

    aula 2 api node - conect banco

--- api-node/config/custom-express.js
+++ api-node/config/custom-express.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const consign = require('consign');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 consign()
     .include('routes')
+    .then('persistencia')
     .into(app);
 
 module.exports = app;
--- api-node/routes/pagamentos.js
+++ api-node/routes/pagamentos.js
@@ -1,5 +1,29 @@
 module.exports = (app) => {
+
     app.get('/pagamentos', function(req, res){
         res.send('Pay Pagamanetos');
+    });
+
+    app.post('/pagamentos/pagamento', function(req, res){
+        let pagamento = req.body;
+        console.log('Criando pagamento');
+
+        pagamento.status = 'Criado';
+        pagamento.data = new Date();
+
+
+        let connection = app.persistencia.connectionFactory();
+        let pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+        pagamentoDao.salva(pagamento, function(exception, result){
+            if(exception) {
+                console.log('erro: ' + exception);
+                res.send('Erro na operação');
+            }
+            else {
+                console.log('pagamento criado: ' + result);
+                res.json(pagamento);
+            }
+        });
     });
 }
