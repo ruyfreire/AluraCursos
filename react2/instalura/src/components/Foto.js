c143c0174182ea90a81946ea899f7440572b8216
Ruy Freire <ruy.slv@gmail.com>
2019-10-06 00:08:29 -0300

    iniciando curso 2 react

---
+++ react2/instalura/src/components/Foto.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+
+import usuario from '../imgs/usuario.jpg';
+import foto from '../imgs/foto.jpg';
+
+export default class Foto extends Component {
+    render() {
+        return (
+            <div className="foto">
+                
+                <FotoHeader/>
+
+                <img alt="foto" className="foto-src" src={foto} />
+
+                <FotoInfo/>
+
+                <FotoAtualizacoes/>
+
+            </div>
+        );
+    }
+}
+
+class FotoHeader extends Component {
+    render() {
+        return (
+            <header className="foto-header">
+                <figure className="foto-usuario">
+                    <img src={usuario}
+                        alt="foto do usuario" />
+                    <figcaption className="foto-usuario">
+                        <a href="#">
+                            Ruy
+                        </a>
+                    </figcaption>
+                </figure>
+                <time className="foto-data">03/10/2016 20:13</time>
+            </header>
+        );
+    }
+}
+
+class FotoInfo extends Component {
+    render() {
+        return (
+            <div className="foto-info">
+                <div className="foto-info-likes">
+                    <a href="#">
+                        alots_ssa
+                    </a>
+                    ,
+                    <a href="#">
+                        rafael_rollo
+                    </a>
+                    curtiram
+                </div>
+
+                <p className="foto-info-legenda">
+                    <a className="foto-info-autor">autor </a>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+                </p>
+
+                <ul className="foto-info-comentarios">
+                    <li className="comentario">
+                        <a className="foto-info-autor">seguidor </a>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
+                    </li>
+                    <li className="comentario">
+                        <a className="foto-info-autor">seguidor </a>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias
+                        voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores
+                        dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
+                    </li>
+                    <li className="comentario">
+                        <a className="foto-info-autor">seguidor </a>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio
+                        delectus maiores voluptatibus sit commodi quidem.
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+class FotoAtualizacoes extends Component {
+    render() {
+        return (
+            <section className="fotoAtualizacoes">
+                <a href="#" className="fotoAtualizacoes-like">Likar</a>
+                <form className="fotoAtualizacoes-form">
+                    <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" />
+                    <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit" />
+                </form>
+            </section>
+        );
+    }
+}
---
+++ react2/instalura/src/components/Timeline.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Foto from './Foto';
+
+export default class Timeline extends Component {
+    render() {
+        return (
+            <div className="fotos container">
+                <Foto/>
+                <Foto/>
+                <Foto/>
+            </div>
+        );
+    }
+}
